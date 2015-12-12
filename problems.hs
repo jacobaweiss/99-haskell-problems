ef4a911133d62e641f34b75c1971303dafcf2dcf
Jack Weiss <jacobaweiss@gmail.com>
2015-12-11 22:34:26 -0800

    Solutions to 11 and 12

--- problems.hs
+++ problems.hs
@@ -37,18 +37,44 @@ compress = map head . group
 
 -- 9) Pack consecutive duplicates of list elements into sublists. If a list contains repeated elements they should be placed in separate sublists.
 pack :: (Eq a) => [a] -> [[a]]
-pack = foldr compress' [] . map wrap
+pack = foldr compress [] . map wrap
   where wrap = flip (:) []
-        compress' x [] = [x]
-        compress' x (y:ys)
-          | head x == head y = compress' (head x : y) ys
-          | otherwise = x:(compress' y ys)
+        compress x [] = [x]
+        compress x (y:ys)
+          | head x == head y = compress (head x : y) ys
+          | otherwise = x:(compress y ys)
 
 -- 10) Run-length encoding of a list. Use the result of problem P09 to implement the so-called run-length encoding data compression method. Consecutive duplicates of elements are encoded as lists (N E) where N is the number of duplicates of the element E.
 encode :: (Eq a) => [a] -> [(Int, a)]
-encode = foldr compress' [] . map encoded
+encode = foldr compress [] . map encoded
   where encoded x = (1,x)
-        compress' t [] = [t]
-        compress' t ((n,y):ys)
-          | snd t == y = compress' (n+1,y) ys
-          | otherwise  = t:(compress' (n,y) ys)
+        compress t [] = [t]
+        compress t ((n,y):ys)
+          | snd t == y = compress (n+1,y) ys
+          | otherwise  = t:(compress (n,y) ys)
+
+-- 11) Modified run-length encoding.
+-- Modify the result of problem 10 in such a way that if an element has no duplicates it is simply copied into the result list. Only elements with duplicates are transferred as (N E) lists.
+data Numerable a = Multiple Int a | Single a deriving (Show)
+
+encodeModified :: (Eq a) => [a] -> [Numerable a]
+encodeModified =  foldr compress [] . map encoded
+  where encoded x = Single x
+        compress x [] = [x]
+        compress (Multiple n x) (Multiple m y:ys) = (Multiple n x):(compress (Multiple m y) ys)
+        compress (Single x) (Single y:ys)
+          | x == y    = compress (Multiple 2 y) ys
+          | otherwise = (Single x):(compress (Single y) ys)
+        compress (Single x) (Multiple n y:ys)
+          | x == y    = compress (Multiple (n+1) y) ys
+          | otherwise = (Single x):(compress (Multiple n y) ys)
+        compress (Multiple n x) (Single y:ys)
+          | x == y    = compress (Multiple (n+1) y) ys
+          | otherwise = (Multiple n x):(compress (Single y) ys)
+
+-- 12) Decode a run-length encoded list.
+-- Given a run-length code list generated as specified in problem 11. Construct its uncompressed version.
+decodeModified :: (Eq a) => [Numerable a] -> [a]
+decodeModified = concat . map decoded
+  where decoded (Single x) = [x]
+        decoded (Multiple n x) = replicate n x
